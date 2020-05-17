1c0968a3398bc70a871d0e7957d9fa00fb863fc8
Alexander <alexkost.f@gmail.com>
2020-05-17 16:51:08 +0300

    It was very hard for me

--- src/components/HeaderBlock/index.js
+++ src/components/HeaderBlock/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import s from './HeaderBlock.module.scss';
-import ReactLogoPng from '../../logo512.png'
-import {ReactComponent as ReactLogoSvg} from '../../logo.svg'
 
-const HeaderBlock = () => {
+const HeaderBlock = ( { hideBackground = false, children } ) => {
+    const styleCover = hideBackground ? { backgroundImage: 'none' } : {};
     return (
-        <div className={s.cover}>
+        <div className={s.cover} style={styleCover}>
             <div className={s.wrap}>
-                <h1 className={s.header}>Учите слова онлайн</h1>
-                <img src={ReactLogoPng} alt="Логотип" />
-                <ReactLogoSvg />
-                <p className="s.decr">Используйте карточки для запоминания и поплняйте активный словарный запас</p>
+                {children}  
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,51 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import HeaderBlock from './components/HeaderBlock';
+import App from './App';
+
 import './index.css';
 
-const AppList = () => {
-  const items = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
-  const firstItem = <li>Item 0</li>;
-
-  const isAuth = true;
-
-  return (
-    <ul>
-      { isAuth ? firstItem : null }
-      { items.map(item => <li>{item}</li>) }
-      <li>{ items[0] }</li>
-      <li>{ items[1] }</li>
-    </ul>
-  );
-}
-
-const AppHeader = () => {
-  return (
-    <h1 className="header">My Header</h1>
-  );
-}
-
-const AppInput = () => {
-  const placeholder = 'Type text...';
-
-  return (
-    <label htmlFor="search">
-      <input placeholder={placeholder} />
-    </label>
-  )
-}
-
-const App = () => {
-  return(
-    <React.Fragment>
-      <HeaderBlock/>
-      <AppHeader></AppHeader>
-      <AppInput />
-      <AppList></AppList>
-      <AppHeader></AppHeader>
-      <AppList></AppList>
-    </React.Fragment>
-  );
-}
 
 ReactDom.render(<App />, document.getElementById('root'));
